1b903517562efac56900840287cda5083a88a54a
Xavier <Xaviermaldonadony@hotmail.com>
2025-10-07 02:40:10 -0400

    login and register component

--- src/auth/layouts/AuthLayout.tsx
+++ src/auth/layouts/AuthLayout.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router';
+
 const AuthLayout = () => {
-  return <div>AuthLayouts</div>;
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10'>
+      <div className='w-full max-w-sm md:max-w-3xl'>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
